fbd1e1afe78397548356d7a9d5b5c633c5a97c0e
dejvidEm <bithug10@gmail.com>
2025-01-29 13:59:42 +0100

    opravy bugov

--- src/components/Partnership.tsx
+++ src/components/Partnership.tsx
@@ -8,9 +8,11 @@ type PartnersProps = {
 
 const Partners: React.FC<PartnersProps> = ({ logos, title }) => {
   return (
-    <div className="relative w-full bg-[#0A2125] py-8 my-28">
-      <h2 className="max-w-[800px] mx-auto text-center text-3xl md:text-6xl font-bold px-10 md:px-0 mb-10 md:mb-16 bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text uppercase">{title}</h2>
-      <div className="flex justify-center items-center space-x-8 over">
+    <div className="relative w-full bg-[#0A2125] py-8 my-28 flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-3xl md:text-6xl font-bold px-10 md:px-0 mb-10 md:mb-16 md:max-w-[750px] bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text uppercase text-center">
+        {title}
+      </h2>
+      <div className="flex justify-center items-center space-x-8">
         {logos.map((logo, index) => (
           <TeamCard key={index} logo={logo} altText={`Logo ${index}`} />
         ))}
--- src/components/PlayerTransfers.tsx
+++ src/components/PlayerTransfers.tsx
@@ -24,29 +24,33 @@ const players: Player[] = [
 ];
 
 const PlayerTransfers: React.FC = () => {
-  const highlightedIndex = 2; // Index riadku, ktorý chceme zvýrazniť (tu napr. tretí hráč)
+  const highlightedIndex = players.findIndex(player => player.name === 'Lionel Messi'); // Prvý Messi
 
   return (
-    <div className="bg-[#0A2125] text-white px-6 py-8">
-        <h1 className='text-3xl md:text-6xl bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text mb-10 uppercase font-bold leading-8'>From Players <br />
-        <span className='ml-16'>to transfers</span></h1>
-      <div className="gap-8">
-        <div className="text-center">
-          <div className="w-full max-w-md mx-auto hidden md:block">
-            <img src="/images/messi.png" alt="Player" className="mx-auto mb-4" />
-          </div>
+    <div className="bg-[#0A2125] text-white px-6 py-8 relative">
+      <h1 className='text-3xl md:text-6xl bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text mb-10 uppercase font-bold leading-8'>
+        From Players <br />
+        <span className='ml-16'>to transfers</span>
+      </h1>
+      <div className="gap-8 relative">
+        {/* VEĽKÁ fotka úplne prekryje podčiarknutie */}
+        <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 z-20 bg-[#0A2125]">
+          <img src="/images/messi.png" alt="Player" className="w-72 h-72" />
         </div>
+        
         <div className="col-span-1 md:col-span-2 lg:col-span-2">
           <div className="flex justify-between text-xl font-semibold mb-8">
-            <span className="text-2xl bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text font-bold">13 PLAYERS</span>
+            <span className="text-2xl bg-gradient-to-r from-gold_secondary to-gold_primary inline-block text-transparent bg-clip-text font-bold">
+              13 PLAYERS
+            </span>
             <span className="text-2xl font-bold">12 TRANSFERS</span>
           </div>
-          <div className="space-y-2">
+          <div className="space-y-2 relative">
             {players.map((player, index) => (
               <div
                 key={index}
                 className={`flex justify-between text-lg ${
-                  index === highlightedIndex ? 'text-gold_primary' : ''
+                  index === highlightedIndex ? 'border-b-2 border-gold_primary relative z-10' : ''
                 }`}
               >
                 <div className="flex flex-row gap-6">
